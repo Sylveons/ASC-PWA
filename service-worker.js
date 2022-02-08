07578e76fb7224d793a83f6a3ccc893593e2269e
Dominique Walker <dw629@live.mdx.ac.uk>
2022-02-08 16:28:06 +0000

    yes

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+var cacheName = 'ASC';
+var cacheFiles = [
+'index.html'
+,
+'ASC.webmanifest'
+,
+'math.png'
+
+,
+'icon.png'
+
+];
+
+self.addEventListener('install'
+, (e) => {
+console.log('[Service Worker] Install');
+e.waitUntil(
+caches.open(cacheName).then((cache) => {
+console.log('[Service Worker] Caching all the files');
+return cache.addAll(cacheFiles);
+})
+);
+});
