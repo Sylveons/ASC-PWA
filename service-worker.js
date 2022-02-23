27882b89f4c9ed710174bbd20d26f515da984faf
Dominique Walker <DW629@live.mdx.ac.uk>
2022-02-23 18:35:05 +0000

    service worker

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,6 @@ var cacheFiles = [
 'math.png'
 
 ,
-'icon.png'
 
 ];
 
@@ -18,6 +17,22 @@ e.waitUntil(
 caches.open(cacheName).then((cache) => {
 console.log('[Service Worker] Caching all the files');
 return cache.addAll(cacheFiles);
+})
+);
+});
+
+self.addEventListener('fetch'
+, function (e) {
+e.respondWith(
+caches.match(e.request).then(function (r) {
+// Download the file if it is not in the cache,
+return r || fetch(e.request).then(function (response) {
+// add the new file to cache
+return caches.open(cacheName).then(function (cache) {
+cache.put(e.request, response.clone());
+return response;
+});
+});
 })
 );
 });
